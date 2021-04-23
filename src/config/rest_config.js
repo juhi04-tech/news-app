ab6e6a753f6b497e28673ad90eab78c1fece5d42
juhi04 <sinhajuhi43@yahoo.com>
2021-04-23 07:55:38 +0530

    API UPDATE

---
+++ src/config/rest_config.js
@@ -0,0 +1,5 @@
+export const  articles_url = " https://gnews.io/api/v4/top-headlines/";
+export const language = "en";
+export const category = "general";
+
+export const _api_key = "lzOygK31DDaJjtlIUN5UtzHbNZg8Q3knlU9jpMcTV2NVWeYdykKlYgzNNDhW";
